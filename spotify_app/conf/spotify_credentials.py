39cb1fa6a5a6dd655cd72cb3745b6ede33cffc01
Luca Passariello <luca.passariello@gmail.com>
2019-05-23 16:17:10 +0100

    Initial commmit with venv, requirements, authentication flow

---
+++ spotify_app/conf/spotify_credentials.py
@@ -0,0 +1,6 @@
+credentials = {
+    "client_id": 'a2405f85bfdc4cf5a9f0e01605691147',
+    "client_secret": '3754bdb7037f45208cb50e08d94ed977',
+    "scope": 'user-read-private user-read-email user-read-playback-state user-read-recently-played playlist-read-private playlist-read-collaborative',
+    "redirect_url": 'http://127.0.0.1:5000/auth/callback'
+}
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from spotify_app import application
+
+if __name__ == '__main__':
+    app.run(debug=True)
