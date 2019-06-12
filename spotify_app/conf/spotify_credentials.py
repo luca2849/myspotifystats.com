0a993d796ff777557b10a8bd26730c7b617958ac
luca2849 <luca.passariello@gmail.com>
2019-06-12 12:53:58 +0100

    Credential fix

--- spotify_app/conf/spotify_credentials.py
+++ spotify_app/conf/spotify_credentials.py
@@ -2,6 +2,6 @@ credentials = {
     "client_id": 'dc7941a617b24b64a6af4c015c303edd',
     "client_secret": 'b0cdd7c031114ce49d2eba95bca54ec6',
     "scope": 'user-top-read user-read-private user-read-email playlist-read-private user-read-birthdate user-read-playback-state user-read-recently-played playlist-read-private playlist-read-collaborative',
-    # "redirect_url": 'http://myspotifystats.com/auth/callback'
-    "redirect_url": 'http://127.0.0.1:5000/auth/callback'
+    "redirect_url": 'http://myspotifystats.com/auth/callback'
+    # "redirect_url": 'http://127.0.0.1:5000/auth/callback'
 }
