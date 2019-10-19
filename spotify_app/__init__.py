c0c1521ab842705eda6254a7e66d0ac5bb712b7d
Luca Passariello <luca@lucapassariello.co.uk>
2019-10-19 23:25:58 +0100

    Served all content by https, updated libraries, added meta theme-color

--- spotify_app/__init__.py
+++ spotify_app/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'GII5o_Bq3nTEz2TQB0ei9w'
 application.config['UPLOAD_FOLDER'] = 'app/static/img'
+application.config.update(dict(
+  PREFERRED_URL_SCHEME = 'https'
+))
 
 from spotify_app.routes import auth_routes
 from spotify_app.routes import main_routes
