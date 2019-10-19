167f1d00ba78798caf21e0288b52fe58be2afaf2
Luca Passariello <luca@lucapassariello.co.uk>
2019-10-19 23:49:18 +0100

    Updated all links to https

--- spotify_app/__init__.py
+++ spotify_app/__init__.py
@@ -7,6 +7,16 @@ application.config['UPLOAD_FOLDER'] = 'app/static/img'
 application.config.update(dict(
   PREFERRED_URL_SCHEME = 'https'
 ))
+def _force_https():
+    # my local dev is set on debug, but on AWS it's not (obviously)
+    # I don't need HTTPS on local, change this to whatever condition you want.
+    if not application.debug: 
+        from flask import _request_ctx_stack
+        if _request_ctx_stack is not None:
+            reqctx = _request_ctx_stack.top
+            reqctx.url_adapter.url_scheme = 'https'
+
+application.before_request(_force_https)
 
 from spotify_app.routes import auth_routes
 from spotify_app.routes import main_routes
